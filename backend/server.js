4c038222a3147e43d484387d0a7c7fb8b0eba3a5
Dolfis123 <kdolfis@gmail.com>
2025-06-25 16:24:56 +0900

    finals-38

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 5000;
 // Konfigurasi CORS untuk Socket.IO
 const io = new Server(server, {
     cors: {
-        origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+        origin: process.env.FRONTEND_URL || 'http://localhost:5174',
         methods: ['GET', 'POST'],
         credentials: true
     }
@@ -42,7 +42,7 @@ queueController.setIoInstance(io);
 
 // Middlewares
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'http://localhost:5174',
     credentials: true
 }));
 app.use(bodyParser.json());
