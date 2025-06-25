4c70b8757ef98e8dfd9f27f45df33a7420ce193d
Dolfis123 <kdolfis@gmail.com>
2025-06-25 17:19:28 +0900

    finals-32

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 5000;
 // Konfigurasi CORS untuk Socket.IO
 const io = new Server(server, {
     cors: {
-        origin: process.env.FRONTEND_URL || 'http://localhost:5174',
+        origin: process.env.FRONTEND_URL || 'https://skydance.life',
         methods: ['GET', 'POST'],
         credentials: true
     }
@@ -42,7 +42,7 @@ queueController.setIoInstance(io);
 
 // Middlewares
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5174',
+    origin: process.env.FRONTEND_URL || 'https://skydance.life',
     credentials: true
 }));
 app.use(bodyParser.json());
