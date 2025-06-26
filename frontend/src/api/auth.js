0c73d936163439237d140859c39f20fd5508bcfb
Dolfis123 <kdolfis@gmail.com>
2025-06-26 12:12:06 +0900

    finals-18

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,7 +1,7 @@
 // src/api/services.js
 import axios from 'axios';
 import authApi from './auth'; 
-import { useNavigate, Link } from "react-router-dom";
+
 const API_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
 
 console.log(`[DEBUG - services.js] API_URL yang digunakan: ${API_URL}`);
