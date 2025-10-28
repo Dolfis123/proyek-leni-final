7ec307bb14c595a66b9716df12668600c71dbbcc
Dolfis123 <kdolfis.com>
2025-10-29 01:24:03 +0900

    LENI-1

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ const PORT = process.env.PORT || 5000;
 // Konfigurasi CORS untuk Socket.IO
 const io = new Server(server, {
     cors: {
-        origin: process.env.FRONTEND_URL || "http://localhost:5173",
+        origin: process.env.FRONTEND_URL || "http://localhost:5175" || "http://localhost:5175",
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     },
@@ -46,7 +46,7 @@ app.use(
     cors({
         origin: [
             process.env.FRONTEND_URL || "https://pengadilannegerimanokwari.pro",
-            "http://localhost:5173",
+            "http://localhost:5175",
         ],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
